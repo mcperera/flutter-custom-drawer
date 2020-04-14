9eb591fa08b829368e112a0bf8e6b1dc62512e43
mcperera <madushanperera3017@gmail.com>
2020-04-15 00:14:41 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:fluttergallery/circular_image.dart';
+import 'package:fluttergallery/menu_page.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttergallery/zoom_scaffold.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Zoom Menu',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  MenuController menuController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    menuController = new MenuController(
+      vsync: this,
+    )..addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    menuController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      builder: (context) => menuController,
+      child: ZoomScaffold(
+        menuScreen: MenuScreen(),
+        contentScreen: Layout(
+            contentBuilder: (cc) => Container(
+              color: Colors.grey[200],
+              child: Container(
+                color: Colors.grey[200],
+              ),
+            )),
+      ),
+    );
+  }
+}
---
+++ lib/menu_page.dart
@@ -0,0 +1,105 @@
+import 'package:fluttergallery/circular_image.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttergallery/zoom_scaffold.dart';
+import 'package:provider/provider.dart';
+
+class MenuScreen extends StatelessWidget {
+  final String imageUrl =
+      "";
+
+  final List<MenuItem> options = [
+    MenuItem(Icons.search, 'Search'),
+    MenuItem(Icons.shopping_basket, 'Basket'),
+    MenuItem(Icons.favorite, 'Discounts'),
+    MenuItem(Icons.code, 'Prom-codes'),
+    MenuItem(Icons.format_list_bulleted, 'Orders'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanUpdate: (details) {
+        //on swiping left
+        if (details.delta.dx < -6) {
+          Provider.of<MenuController>(context, listen: true).toggle();
+        }
+      },
+      child: Container(
+        padding: EdgeInsets.only(
+            top: 62,
+            left: 32,
+            bottom: 8,
+            right: MediaQuery.of(context).size.width / 2.9),
+        color: Colors.purple,
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 16),
+                  child: CircularImage(
+                    NetworkImage(imageUrl),
+                  ),
+                ),
+                Text(
+                  'Madushan',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                )
+              ],
+            ),
+            Spacer(),
+            Column(
+              children: options.map((item) {
+                return ListTile(
+                  leading: Icon(
+                    item.icon,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                  title: Text(
+                    item.title,
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                );
+              }).toList(),
+            ),
+            Spacer(),
+            ListTile(
+              onTap: () {},
+              leading: Icon(
+                Icons.settings,
+                color: Colors.white,
+                size: 20,
+              ),
+              title: Text('Settings',
+                  style: TextStyle(fontSize: 14, color: Colors.white)),
+            ),
+            ListTile(
+              onTap: () {},
+              leading: Icon(
+                Icons.headset_mic,
+                color: Colors.white,
+                size: 20,
+              ),
+              title: Text('Support',
+                  style: TextStyle(fontSize: 14, color: Colors.white)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MenuItem {
+  String title;
+  IconData icon;
+
+  MenuItem(this.icon, this.title);
+}
